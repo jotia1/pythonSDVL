25e0d599652c4313e037e5f26f07bfa83b3ee388
Joshua Arnold <joshua.arnold1@uqconnect.edu.au>
2020-04-09 23:43:38 +1000

    Added actual simulation

--- runexperiment.py
+++ runexperiment.py
@@ -1,5 +1,8 @@
 import sys
 from runner import *
+from networks import *
+from simulation import *
+import numpy as np
 
 def main():
 
@@ -17,6 +20,24 @@ def main():
 
     print(f'(value, repeat) : ({value}, {repeat})')
 
+    net = Network()
+    
+    # Set network variable
+    setattr(net, exp_params['variable'], value)
+    net.repeat = repeat
+
+    sim_params = SimulationParameters()
+    sim_params.sim_time_sec = 3
+    sim_params.time_execution = True
+    sim_params.inp_idxs = np.random.randint(0, net.N-1, net.N * 10 * sim_params.sim_time_sec)
+    sim_params.inp_ts = np.random.randint(0, sim_params.sim_time_sec * MSPERSEC, net.N * 10 * sim_params.sim_time_sec)
+    sim_params.voltages_to_save = np.array(net.N-1, dtype=np.int32)
+    sim_params.delays_to_save = np.array([], dtype=np.int32)
+    sim_params.variances_to_save = np.array([], dtype=np.int32)
+
+    out = simulate(net, sim_params)
+
+    print(out.sim_timer.get_percentages())
 
 if __name__ == '__main__':
     main()
